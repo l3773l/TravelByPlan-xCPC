0fb3f5e92457fa1b12bb81da654949d9d57d35ae
l3773l <1437061699@qq.com>
2024-11-03 16:02:00 +0800

    2024 Harbin Regional L 补题

--- CCPC/2024 China Collegiate Programming Contest (CCPC) Harbin Onsite (The 3rd Universal Cup. Stage 14 Harbin)/L.cpp
+++ CCPC/2024 China Collegiate Programming Contest (CCPC) Harbin Onsite (The 3rd Universal Cup. Stage 14 Harbin)/L.cpp
@@ -11,107 +11,91 @@ using ll = long long;
 
 const int mod = 998244353;
 
-int mul(int x, int y) {
-    return 1ll * x * y % mod;
+struct Z {
+    int v;
+    Z() : v(0) {}
+    Z(int x) : v((x %= mod, (x < 0 ? x + mod : x))) {}
+    Z(ll x) : v((x %= mod, (x < 0 ? x + mod : x))) {}
+    Z operator*=(Z y) {
+        v = 1ll * v * y.v % mod; return *this;
+    }
+    Z operator+=(Z y) {
+        if ((v += y.v) >= mod) v -= mod; return *this;
+    }
+    Z operator-=(Z y) {
+        if ((v -= y.v) < 0) v += mod; return *this;
+    }
+    Z operator+(Z y) { Z r = *this; return r += y; }
+    Z operator-(Z y) { Z r = *this; return r -= y; }
+    Z operator*(Z y) { Z r = *this; return r *= y; }
+    Z qpow(Z x, ll y) {
+        Z r = 1 % mod;
+        for (; y; x *= x, y >>= 1) if (y & 1) r *= x;
+        return r;
+    }
+    Z inv() { return qpow(v, mod - 2); }
+    Z operator==(Z y) { return v == y.v; }
+    Z operator/=(Z y) { *this *= y.inv(); return *this; }
+    Z operator/(Z y) { Z r = *this; return r /= y; }
+
+};    
+ostream &operator<<(ostream &os, Z x) {
+    os << x.v;
+    return os;
 }
 
-int add(int x, int y) {
-    int r = x + y;
-    if (r >= mod) r -= mod;
-    return r;
-}
+Z sq(Z x) { return x * x; }
 
-int sub(int x, int y) {
-    int r = x - y;
-    if (r < 0) r += mod;
-    return r;
-}
-
-int qpow(int x, ll y) {
-    int r = 1;
-    for (; y; x = mul(x, x), y >>= 1) if (y & 1) r = mul(r, x);
-    return r;
-}
-
-int inv(int x) {
-    return qpow(x, mod - 2);
-}
+Z one(1);
 
 const int N = 1e5 + 7;
 
 int n;
 
-int sz[N];
-
 vector<int> e[N];
 
-void dfs1(int u, int fa) {
-    sz[u] = 1;
+Z ans;
+
+pair<Z, Z> dfs(int u, int fa) {
+    Z szu = 1;
+    Z sos_szu = 0;
+    Z s_sos_szv = 0, sos_sos_szv = 0;
     for (auto v : e[u]) {
         if (v != fa) {
-            dfs1(v, u);
-            sz[u] += sz[v];
+            auto [szv, sos_szv] = dfs(v, u);
+            szu += szv;
+            sos_szu += sos_szv;
+            Z cb1 = sq(szv) * sq(szv - n);
+            Z cb2 = Z(2) * (sos_szv - sq(szv)) * sq(szv - n);
+            s_sos_szv += sos_szv;
+            sos_sos_szv += sq(sos_szv);
+            ans += cb1 + cb2;
         }
     }
-}
-
-int ans1, ans2;
-
-int mem[N], pmem[N];
-
-int calc(int sumsv2, int su) {
-    return sub(mul(su, su), sumsv2);
-}
-
-void dfs2(int u, int fa) {
-    int ssv2 = 0;
-    for (auto v : e[u]) {
-        if (v != fa) {
-            ssv2 = add(ssv2, mul(sz[v], sz[v]));
-        }
-    }
-
-    mem[u] = calc(ssv2, sz[u]);
-
-    int fsz2 = mul(sub(n, sz[u]), sub(n, sz[u]));
-    int base = add(ssv2, fsz2);
-    for (auto v : e[u]) {
-        if (v != fa) {
-            int szv2 = mul(sz[v], sz[v]);
-            int fssv2 = sub(base, szv2);
-            pmem[v] = calc(fssv2, sz[u]);
-            dfs2(v, u);
-        }
-    }
-
-    ans1 = add(ans1, mul(mem[u], pmem[u]));
-
-    int smemv = 0, smemv2 = 0;
-    for (auto v : e[u]) {
-        if (v != fa) {
-            smemv = add(smemv, mem[v]);
-            smemv2 = add(smemv2, mul(mem[v], mem[v]));
-        }
-    }
-
-    int contrib = add(mul(pmem[u], smemv), mul(sub(mul(smemv, smemv), smemv2), inv(2)));
-    ans2 = add(ans2, contrib);
+    sos_szu += sq(szu);
+    ans += sq(s_sos_szv) - sos_sos_szv;
+    return {szu, sos_szu};
 }
 
 #define pb push_back
 
 void sol() {
     cin >> n;
+
+    For (i, 1, n) e[i].clear();
+
     For (i, 1, n - 1) {
         int u, v;
         cin >> u >> v;
         e[u].pb(v), e[v].pb(u);
     }
+    
+    ans = 0;
+    dfs(1, 0);
 
-    dfs1(1, 0);
-    dfs2(1, 0);
+    Z denom = sq(one * n * (n - 1) / 2);
 
-    cout << add(ans1, ans2) << endl;    
+    cout << (ans / denom).v << endl;
 }
 
 int main() {
